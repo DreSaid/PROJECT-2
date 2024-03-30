04d77efa6ee5225d270edf5ce82bac70f4361582
DreSaid <145886577+DreSaid@users.noreply.github.com>
2024-03-29 23:18:11 -0700

    Add files via upload

---
+++ FINAL/CONTROLLER/CONTROLLER.c
@@ -0,0 +1,408 @@
+#include <XC.h>
+#include <sys/attribs.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/* Pinout for DIP28 PIC32MX130:
+                                          --------
+                                   MCLR -|1     28|- AVDD 
+  VREF+/CVREF+/AN0/C3INC/RPA0/CTED1/RA0 -|2     27|- AVSS 
+        VREF-/CVREF-/AN1/RPA1/CTED2/RA1 -|3     26|- AN9/C3INA/RPB15/SCK2/CTED6/PMCS1/RB15
+   PGED1/AN2/C1IND/C2INB/C3IND/RPB0/RB0 -|4     25|- CVREFOUT/AN10/C3INB/RPB14/SCK1/CTED5/PMWR/RB14
+  PGEC1/AN3/C1INC/C2INA/RPB1/CTED12/RB1 -|5     24|- AN11/RPB13/CTPLS/PMRD/RB13
+   AN4/C1INB/C2IND/RPB2/SDA2/CTED13/RB2 -|6     23|- AN12/PMD0/RB12
+     AN5/C1INA/C2INC/RTCC/RPB3/SCL2/RB3 -|7     22|- PGEC2/TMS/RPB11/PMD1/RB11
+                                    VSS -|8     21|- PGED2/RPB10/CTED11/PMD2/RB10
+                     OSC1/CLKI/RPA2/RA2 -|9     20|- VCAP
+                OSC2/CLKO/RPA3/PMA0/RA3 -|10    19|- VSS
+                         SOSCI/RPB4/RB4 -|11    18|- TDO/RPB9/SDA1/CTED4/PMD3/RB9
+         SOSCO/RPA4/T1CK/CTED9/PMA1/RA4 -|12    17|- TCK/RPB8/SCL1/CTED10/PMD4/RB8
+                                    VDD -|13    16|- TDI/RPB7/CTED3/PMD5/INT0/RB7
+                    PGED3/RPB5/PMD7/RB5 -|14    15|- PGEC3/RPB6/PMD6/RB6
+                                          --------
+*/
+ 
+// Configuration Bits (somehow XC32 takes care of this)
+#pragma config FNOSC = FRCPLL       // Internal Fast RC oscillator (8 MHz) w/ PLL
+#pragma config FPLLIDIV = DIV_2     // Divide FRC before PLL (now 4 MHz)
+#pragma config FPLLMUL = MUL_20     // PLL Multiply (now 80 MHz)
+#pragma config FPLLODIV = DIV_2     // Divide After PLL (now 40 MHz)
+ 
+#pragma config FWDTEN = OFF         // Watchdog Timer Disabled
+#pragma config FPBDIV = DIV_1       // PBCLK = SYCLK
+#pragma config FSOSCEN = OFF 
+// Defines
+#define SYSCLK 40000000L
+#define DEF_FREQ 16000L
+#define Baud2BRG(desired_baud)( (SYSCLK / (16*desired_baud))-1)
+#define Baud1BRG(desired_baud)( (SYSCLK / (16*desired_baud))-1)
+#define FREQ 2048L // 2Hz or 0.5 seconds interrupt rate
+
+void wait_1us(void);
+void delayus(int);
+
+void UART2Configure(int baud_rate)
+{
+    // Peripheral Pin Select
+    U2RXRbits.U2RXR = 4;    //SET RX to RB8
+    RPB9Rbits.RPB9R = 2;    //SET RB9 to TX
+
+    U2MODE = 0;         // disable autobaud, TX and RX enabled only, 8N1, idle=HIGH
+    U2STA = 0x1400;     // enable TX and RX
+    U2BRG = Baud2BRG(baud_rate); // U2BRG = (FPb / (16*baud)) - 1
+    
+    U2MODESET = 0x8000;     // enable UART2
+}
+
+void __ISR(_TIMER_1_VECTOR, IPL5SOFT) Timer1_Handler(void)
+{
+	LATAbits.LATA1 = !LATAbits.LATA1; // Blink led on RB6
+	IFS0CLR = _IFS0_T1IF_MASK; // Clear timer 1 interrupt flag, bit 4 of IFS0
+}
+
+void SetupTimer1 (void)
+{
+	// Explanation here:
+	// https://www.youtube.com/watch?v=bu6TTZHnMPY
+	__builtin_disable_interrupts();
+	PR1 =(SYSCLK/(FREQ*256))-1; // since SYSCLK/FREQ = PS*(PR1+1)
+	TMR1 = 0;
+	T1CONbits.TCKPS = 3; // Pre-scaler: 256
+	T1CONbits.TCS = 0; // Clock source
+	T1CONbits.ON = 1;
+	IPC1bits.T1IP = 5;
+	IPC1bits.T1IS = 0;
+	IFS0bits.T1IF = 0;
+	IEC0bits.T1IE = 1;
+
+	INTCONbits.MVEC = 1; //Int multi-vector
+	__builtin_enable_interrupts();
+}
+
+
+// Needed to by scanf() and gets()
+int _mon_getc(int canblock)
+{
+	char c;
+	
+    if (canblock)
+    {
+	    while( !U2STAbits.URXDA); // wait (block) until data available in RX buffer
+	    c=U2RXREG;
+        while( U2STAbits.UTXBF);    // wait while TX buffer full
+        U2TXREG = c;          // echo
+	    if(c=='\r') c='\n'; // When using PUTTY, pressing <Enter> sends '\r'.  Ctrl-J sends '\n'
+		return (int)c;
+    }
+    else
+    {
+        if (U2STAbits.URXDA) // if data available in RX buffer
+        {
+		    c=U2RXREG;
+		    if(c=='\r') c='\n';
+			return (int)c;
+        }
+        else
+        {
+            return -1; // no characters to return
+        }
+    }
+}
+
+
+void ADCConf(void)
+{
+    AD1CON1CLR = 0x8000;    // disable ADC before configuration
+    AD1CON1 = 0x00E0;       // internal counter ends sampling and starts conversion (auto-convert), manual sample
+    AD1CON2 = 0;            // AD1CON2<15:13> set voltage reference to pins AVSS/AVDD
+    AD1CON3 = 0x0f01;       // TAD = 4*TPB, acquisition time = 15*TAD 
+    AD1CON1SET=0x8000;      // Enable ADC
+}
+
+int ADCRead(char analogPIN)
+{
+    AD1CHS = analogPIN << 16;    // AD1CHS<16:19> controls which analog pin goes to the ADC
+ 
+    AD1CON1bits.SAMP = 1;        // Begin sampling
+    while(AD1CON1bits.SAMP);     // wait until acquisition is done
+    while(!AD1CON1bits.DONE);    // wait until conversion done
+ 
+    return ADC1BUF0;             // result stored in ADC1BUF0
+}
+
+
+
+/////////////////////////////////////////////////////////
+// UART1 functions used to communicate with the JDY40  //
+/////////////////////////////////////////////////////////
+
+// TXD1 is in pin 26  aka RXD aka RB15
+// RXD1 is in pin 24  aka TXD aka RB13
+
+int UART1Configure(int desired_baud)
+{
+	int actual_baud;
+
+    // Peripheral Pin Select for UART1.  These are the pins that can be used for U1RX from TABLE 11-1 of '60001168J.pdf':
+    // 0000 = RPA2
+	// 0001 = RPB6
+	// 0010 = RPA4
+	// 0011 = RPB13
+	// 0100 = RPB2
+
+	// Do what the caption of FIGURE 11-2 in '60001168J.pdf' says: "For input only, PPS functionality does not have
+    // priority over TRISx settings. Therefore, when configuring RPn pin for input, the corresponding bit in the
+    // TRISx register must also be configured for input (set to ‘1’)."
+    
+    ANSELB &= ~(1<<13); // Set RB13 as a digital I/O
+    TRISB |= (1<<13);   // configure pin RB13 as input
+    CNPUB |= (1<<13);   // Enable pull-up resistor for RB13
+    U1RXRbits.U1RXR = 3; // SET U1RX to RB13
+
+    // These are the pins that can be used for U1TX. Check table TABLE 11-2 of '60001168J.pdf':
+    // RPA0
+	// RPB3
+	// RPB4
+	// RPB15
+	// RPB7
+
+    ANSELB &= ~(1<<15); // Set RB15 as a digital I/O
+    RPB15Rbits.RPB15R = 1; // SET RB15 to U1TX
+	
+    U1MODE = 0;         // disable autobaud, TX and RX enabled only, 8N1, idle=HIGH
+    U1STA = 0x1400;     // enable TX and RX
+    U1BRG = Baud1BRG(desired_baud); // U1BRG = (FPb / (16*baud)) - 1
+    // Calculate actual baud rate
+    actual_baud = SYSCLK / (16 * (U1BRG+1));
+
+    U1MODESET = 0x8000;     // enable UART1
+
+    return actual_baud;
+}
+ 
+int SerialTransmit1(const char *buffer)
+{
+    unsigned int size = strlen(buffer);
+    while(size)
+    {
+        while( U1STAbits.UTXBF);    // wait while TX buffer full
+        U1TXREG = *buffer;          // send single character to transmit buffer
+        buffer++;                   // transmit next character on following loop
+        size--;                     // loop until all characters sent (when size = 0)
+    }
+ 
+    while( !U1STAbits.TRMT);        // wait for last transmission to finish
+ 
+    return 0;
+}
+ 
+unsigned int SerialReceive1(char *buffer, unsigned int max_size)
+{
+    unsigned int num_char = 0;
+ 	int timeout_counter;
+ 
+    while(num_char < max_size)
+    {
+    	timeout_counter=0;
+        while( !U1STAbits.URXDA)
+        {
+        	delayus(100);
+        	timeout_counter++;
+        	if(timeout_counter==100)
+        	{
+        		printf("TIMEOUT");
+        		*buffer = '\0';
+        		return num_char;
+        	}
+        }   // wait until data available in RX buffer
+        *buffer = U1RXREG;          // empty contents of RX buffer into *buffer pointer
+ 
+        // insert nul character to indicate end of string
+        if( *buffer == '\n')
+        {
+            *buffer = '\0';     
+            break;
+        }
+ 
+        buffer++;
+        num_char++;
+    }
+ 
+    return num_char;
+}
+
+// Use the core timer to wait for 1 ms.
+void wait_1ms(void)
+{
+    unsigned int ui;
+    _CP0_SET_COUNT(0); // resets the core timer count
+
+    // get the core timer count
+    while ( _CP0_GET_COUNT() < (SYSCLK/(2*1000)) );
+}
+
+void delayms(int len)
+{
+	while(len--) wait_1ms();
+}
+
+void wait_1us(void)
+{
+    unsigned int ui;
+    _CP0_SET_COUNT(0); // resets the core timer count
+
+    // get the core timer count
+    while ( _CP0_GET_COUNT() < (SYSCLK/(2*1000000)) );
+}
+
+void delayus(int len)
+{
+	while(len--) wait_1us();
+}
+
+void SendATCommand (char * s)
+{
+	char buff[40];
+	printf("Command: %s", s);
+	LATB &= ~(1<<14); // 'SET' pin of JDY40 to 0 is 'AT' mode.
+	delayms(10);
+	SerialTransmit1(s);
+	SerialReceive1(buff, sizeof(buff)-1);
+	LATB |= 1<<14; // 'SET' pin of JDY40 to 1 is normal operation mode.
+	delayms(10);
+	printf("Response: %s\n", buff);
+}
+
+void main(void)
+{
+	char buff[80];
+    int cnt=0;
+    volatile unsigned long t=0;
+    int adcval;
+    float voltage;
+    float voltage_x;
+    float voltage_y;
+    int adcval_x;
+    int adcval_y;
+    int a;
+    int speaker = 1;
+    int timeout;
+    int timeout_cnt = 0;
+    int received_value; //this is a TESTING variable
+    
+    
+ 	
+    
+	DDPCON = 0;
+	CFGCON = 0;
+	
+	ANSELAbits.ANSA1 = 0; // Disable analog function on RA1
+    TRISAbits.TRISA1 = 0; // Set RA1 as output
+    LATAbits.LATA1 = 0;   // Initialize RA1 to low
+
+    INTCONbits.MVEC = 1; // Enable multi-vector interrupts
+	SetupTimer1();
+	
+	// Configure pins as analog inputs
+    ANSELBbits.ANSB1 = 1;   // set RB1 (AN4, pin 5 of DIP28) as analog pin	
+    TRISBbits.TRISB1 = 1;   // set RB1 as an input
+    
+    ANSELBbits.ANSB2 = 1;
+    TRISBbits.TRISB2 = 1;	//set RB2 as input (x direction)
+	
+	ADCConf(); // Configure ADC
+
+  
+    UART2Configure(115200);  // Configure UART2 for a baud rate of 115200
+    UART1Configure(9600);  // Configure UART1 to communicate with JDY40 with a baud rate of 9600
+ 
+	delayms(500); // Give putty time to start before we send stuff.
+    printf("JDY40 test program.\r\n");
+
+	// RB14 is connected to the 'SET' pin of the JDY40.  Configure as output:
+    ANSELB &= ~(1<<14); // Set RB14 as a digital I/O
+    TRISB &= ~(1<<14);  // configure pin RB14 as output
+	LATB |= (1<<14);    // 'SET' pin of JDY40 to 1 is normal operation mode
+
+	// We should select an unique device ID.  The device ID can be a hex
+	// number from 0x0000 to 0xFFFF.  In this case is set to 0xABBA
+	SendATCommand("AT+DVID6969\r\n");  
+
+	// To check configuration
+	SendATCommand("AT+VER\r\n");
+	SendATCommand("AT+BAUD\r\n");
+	SendATCommand("AT+RFID\r\n");
+	SendATCommand("AT+DVID\r\n");
+	SendATCommand("AT+RFC\r\n");
+	SendATCommand("AT+POWE\r\n");
+	SendATCommand("AT+CLSS\r\n");           
+
+    ANSELB &= ~(1<<6); // Set RB6 as a digital I/O
+    TRISB |= (1<<6);   // configure pin RB6 as input
+    CNPUB |= (1<<6);   // Enable pull-up resistor for RB6
+    
+    ANSELA &= ~(1<<1);	//Set RA1 as a digital I/O
+    TRISA &= ~(1<<1); 	//configure pin RA1 as output (speaker)
+ 	
+	printf("\r\nPress and hold a push-button attached to RB6 (pin 15) to transmit.\r\n");
+	
+	cnt=0;
+	while(1)
+	{
+		timeout=0;
+		adcval_y = ADCRead(4); // note that we call pin AN4 (RB2) by it's analog number
+    	voltage_y=adcval_y*3.3/1023.0;
+    	adcval_x = ADCRead(3); 	//reading from AN3 (RB1)
+    	voltage_x = adcval_x*3.3/1023.0;
+    	
+    //	printf("%.3f %.3f\r", voltage_y, voltage_x);
+ 	  	 // Makes the printf() above to send without a '\n' at the end
+		t = 0;
+		a=0;
+		
+		sprintf(buff, "%.3f\n%.3f\r\n", voltage_y, voltage_x);
+		printf("%d", strlen(buff));
+		printf("line %s\n", buff);
+		SerialTransmit1(buff);
+		
+	//	fflush(stdout);
+		
+		delayms(20);	
+			
+		
+		if((PORTB&(1<<6))==0)
+		{
+		sprintf(buff, "M\r\n");	
+		SerialTransmit1(buff);
+	
+		delayms(10);	
+			
+		}
+		while(1) {
+			if(U1STAbits.URXDA) break; //Got something! get out of loop
+			delayus(100); //check if smth has arrived
+			timeout_cnt++;
+			if(timeout_cnt>=100) break; //timeout after 10 ms
+			//printf("stuck1");
+		}
+		if(U1STAbits.URXDA) {// Something has arrived
+			//printf("arrived");
+			//delayms(100);
+			SerialReceive1(buff, sizeof(buff)-1);
+			//printf("Received_val: %s\r\n", buff);
+			//printf("received\r\n");
+			if(strlen(buff)==5) //assuming a message from robot is 5 bytes
+			{
+				//printf("in\n");
+				received_value = atoi(buff);
+				printf("%d\r\n",received_value);
+
+			}
+		}
+		if(speaker)
+			LATA &= ~(1<<1);
+		
+	}
+}
---
+++ FINAL/ROBOT/ROBOT.c
@@ -0,0 +1,373 @@
+#include "../Common/Include/stm32l051xx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../Common/Include/serial.h"
+#include "UART2.h"
+#include <math.h>
+#include <string.h>
+
+#define F_CPU 32000000L
+#define SYSCLK 32000000L
+#define DEF_F 100000L
+
+volatile int PWM_Counter = 0;
+volatile unsigned char rightf_pwm=0, rightb_pwm=0, leftf_pwm=0, leftb_pwm=0;
+
+// LQFP32 pinout
+//             ----------
+//       VDD -|1       32|- VSS
+//      PC14 -|2       31|- BOOT0
+//      PC15 -|3       30|- PB7
+//      NRST -|4       29|- PB6
+//      VDDA -|5       28|- PB5
+//       PA0 -|6       27|- PB4
+//       PA1 -|7       26|- PB3
+//       PA2 -|8       25|- PA15 (Used for RXD of UART2, connects to TXD of JDY40)
+//       PA3 -|9       24|- PA14 (Used for TXD of UART2, connects to RXD of JDY40)
+//       PA4 -|10      23|- PA13 (Used for SET of JDY40)
+//	 PA5 -|11      22|- PA12
+//       PA6 -|12      21|- PA11
+//pbuttonPA7 -|13      20|- PA10 (Reserved for RXD of UART1)
+//       PB0 -|14      19|- PA9  (Reserved for TXD of UART1)
+//       PB1 -|15      18|- PA8  
+//       VSS -|16      17|- VDD
+//             ----------
+
+#define F_CPU 32000000L
+// Uses SysTick to delay <us> micro-seconds. 
+void Delay_us(unsigned char us)
+{
+	// For SysTick info check the STM32L0xxx Cortex-M0 programming manual page 85.
+	SysTick->LOAD = (F_CPU/(1000000L/us)) - 1;  // set reload register, counter rolls over from zero, hence -1
+	SysTick->VAL = 0; // load the SysTick counter
+	SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk; // Enable SysTick IRQ and SysTick Timer */
+	while((SysTick->CTRL & BIT16)==0); // Bit 16 is the COUNTFLAG.  True when counter rolls over from zero.
+	SysTick->CTRL = 0x00; // Disable Systick counter
+}
+
+
+
+// Interrupt service routines are the same as normal
+// subroutines (or C funtions) in Cortex-M microcontrollers.
+// The following should happen at a rate of 1kHz.
+// The following function is associated with the TIM2 interrupt 
+// via the interrupt vector table defined in startup.c
+void TIM2_Handler(void) 
+{
+	TIM2->SR &= ~BIT0; // clear update interrupt flag
+	PWM_Counter++;
+	
+	if(rightf_pwm>PWM_Counter)
+	{
+		GPIOA->ODR |= BIT1;
+//		GPIOA->ODR |= BIT3;
+	}
+	else
+	{
+		GPIOA->ODR &= ~BIT1;
+//		GPIOA->ODR &= ~BIT3;
+	}
+	
+	if(leftf_pwm>PWM_Counter)
+	{
+//		GPIOA->ODR |= BIT1;
+		GPIOA->ODR |= BIT3;
+	}
+	else
+	{
+//		GPIOA->ODR &= ~BIT1;
+		GPIOA->ODR &= ~BIT3;
+	}
+	
+	if(rightb_pwm>PWM_Counter)
+	{
+		GPIOA->ODR |= BIT4;
+//		GPIOA->ODR |= BIT2;
+	}
+	else
+	{
+		GPIOA->ODR &= ~BIT4;
+//		GPIOA->ODR &= ~BIT2;
+	}
+	
+	if(leftb_pwm>PWM_Counter)
+	{
+//		GPIOA->ODR |= BIT4;
+		GPIOA->ODR |= BIT2;
+	}
+	else
+	{
+//		GPIOA->ODR &= ~BIT4;
+		GPIOA->ODR &= ~BIT2;
+	}
+	
+	if (PWM_Counter > 255) // THe period is 20ms
+	{
+		PWM_Counter=0;
+		GPIOA->ODR |= (BIT4|BIT1);
+		GPIOA->ODR |= (BIT2|BIT3);
+	}   
+}
+
+void wait_1ms(void)
+{
+	// For SysTick info check the STM32l0xxx Cortex-M0 programming manual.
+	SysTick->LOAD = (F_CPU/1000L) - 1;  // set reload register, counter rolls over from zero, hence -1
+	SysTick->VAL = 0; // load the SysTick counter
+	SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk; // Enable SysTick IRQ and SysTick Timer */
+	while((SysTick->CTRL & BIT16)==0); // Bit 16 is the COUNTFLAG.  True when counter rolls over from zero.
+	SysTick->CTRL = 0x00; // Disable Systick counter
+}
+
+void waitms (unsigned int ms)
+{
+	unsigned int j;
+	unsigned char k;
+	for(j=0; j<ms; j++)
+		for (k=0; k<4; k++) Delay_us(250);
+}
+
+void Hardware_Init(void)
+{
+	GPIOA->OSPEEDR=0xffffffff; // All pins of port A configured for very high speed! Page 201 of RM0451
+
+	RCC->IOPENR |= BIT0; // peripheral clock enable for port A
+
+    GPIOA->MODER = (GPIOA->MODER & ~(BIT27|BIT26)) | BIT26; // Make pin PA13 output (page 200 of RM0451, two bits used to configure: bit0=1, bit1=0))
+	GPIOA->ODR |= BIT13; // 'set' pin to 1 is normal operation mode.
+
+	GPIOA->MODER &= ~(BIT14 | BIT15); // Make pin PA8 input
+	// Activate pull up for pin PA8:
+	
+	GPIOA->MODER = (GPIOA->MODER & ~(BIT8|BIT9)) | BIT8; // Make pin PA4 output (page 200 of RM0451, two bits used to configure: bit0=1, bit1=0)
+	GPIOA->OTYPER &= ~BIT4; // Push-pull
+    GPIOA->MODER = (GPIOA->MODER & ~(BIT2|BIT3)) | BIT2; // Make pin PA1 output (page 200 of RM0451, two bits used to configure: bit0=1, bit1=0)
+	GPIOA->OTYPER &= ~BIT1; // Push-pull
+	
+	GPIOA->MODER = (GPIOA->MODER & ~(BIT6|BIT7)) | BIT6; // Make pin PA3 output (page 200 of RM0451, two bits used to configure: bit0=1, bit1=0)
+	GPIOA->OTYPER &= ~BIT3; // Push-pull
+    GPIOA->MODER = (GPIOA->MODER & ~(BIT4|BIT5)) | BIT4; // Make pin PA2 output (page 200 of RM0451, two bits used to configure: bit0=1, bit1=0)
+	GPIOA->OTYPER &= ~BIT2; // Push-pull
+	
+	GPIOA->PUPDR |= BIT14; 
+	GPIOA->PUPDR &= ~(BIT15);
+	
+	// Set up timer
+	RCC->APB1ENR |= BIT0;  // turn on clock for timer2 (UM: page 177)
+	TIM2->ARR = F_CPU/DEF_F-1;
+	NVIC->ISER[0] |= BIT15; // enable timer 2 interrupts in the NVIC
+	TIM2->CR1 |= BIT4;      // Downcounting    
+	TIM2->CR1 |= BIT7;      // ARPE enable    
+	TIM2->DIER |= BIT0;     // enable update event (reload event) interrupt 
+	TIM2->CR1 |= BIT0;      // enable counting    
+	
+	__enable_irq();
+}
+
+void SendATCommand (char * s)
+{
+	char buff[40];
+	printf("Command: %s", s);
+	GPIOA->ODR &= ~(BIT13); // 'set' pin to 0 is 'AT' mode.
+	waitms(10);
+	eputs2(s);
+	egets2(buff, sizeof(buff)-1);
+	GPIOA->ODR |= BIT13; // 'set' pin to 1 is normal operation mode.
+	waitms(10);
+	printf("Response: %s", buff);
+}
+
+int main(void)
+{
+	char x_voltage [80];
+	char y_voltage [80];
+	float y_volts;
+    float x_volts;
+	
+    float y_power;
+    float x_power;
+    float power;
+	
+	char metal_reading [80];
+	float metal;
+	
+    int cnt=0;
+    float temp;
+    char buff[80];
+
+
+	Hardware_Init();
+	initUART2(9600);
+	
+	waitms(1000); // Give putty some time to start.
+	printf("\r\nJDY-40 test\r\n");
+
+	// We should select an unique device ID.  The device ID can be a hex
+	// number from 0x0000 to 0xFFFF.  In this case is set to 0xABBA
+	SendATCommand("AT+DVID6969\r\n");  
+
+	// To check configuration
+	SendATCommand("AT+VER\r\n");
+	SendATCommand("AT+BAUD\r\n");
+	SendATCommand("AT+RFID\r\n");
+	SendATCommand("AT+DVID\r\n");
+	SendATCommand("AT+RFC\r\n");
+	SendATCommand("AT+POWE\r\n");
+	SendATCommand("AT+CLSS\r\n");
+	
+	printf("\r\nPress and hold a push-button attached to PA8 (pin 18) to transmit.\r\n");
+	
+	cnt=0;
+	while(1)
+	{
+	//	printf("PWM1 (60 to 255): ");
+    //	fflush(stdout);
+    //	egets_echo(buf, 31); // wait here until data is received
+  	//	printf("\r\n");
+	    
+	    
+    //	printf("PWM2 (60 to 255): ");
+    //	fflush(stdout);
+    //	egets_echo(buf, 31); // wait here until data is received
+ 	//	printf("\r\n");
+ 
+
+		if((GPIOA->IDR&BIT7)==0)
+		{
+			sprintf(buff, "JDY40 test %d\n", cnt++);
+			eputs2(buff);
+			eputc('.');
+			waitms(200);
+		}
+		
+		if(ReceivedBytes2()>0) // Something has arrived
+		{
+			
+			
+				egets2(y_voltage, sizeof(y_voltage)-1);
+				egets2(x_voltage, sizeof(x_voltage)-1);
+				
+				printf("Y%s\n\rX%s\n\r",y_voltage,x_voltage);
+				printf("Y%i\n\rX%i\n\r", strlen(y_voltage), strlen(x_voltage));
+				
+				if(y_voltage[0] == 'M')
+				{
+					if((strlen(x_voltage) == 7) && (strlen(y_voltage) == 7))
+					{
+						if(((atof(x_voltage)<=3.4) && (atof(x_voltage)>=0)) && ((atof(y_voltage)<=3.4) && (atof(y_voltage)>=0)))
+						{			
+							x_volts = atof(x_voltage);
+							y_volts = atof(y_voltage + 1);
+							
+							//sprintf(metal_reading
+							//eputs2(metal_reading);
+						}
+					}
+				}
+				else
+				{
+					if((strlen(x_voltage) == 7) && (strlen(y_voltage) == 6))
+					{
+						if(((atof(x_voltage)<=3.4) && (atof(x_voltage)>=0)) && ((atof(y_voltage)<=3.4) && (atof(y_voltage)>=0)))
+						{			
+							x_volts = atof(x_voltage);
+							y_volts = atof(y_voltage);
+						}
+					}
+				}
+				
+				//x_volts = atof(x_voltage);
+				//y_volts = atof(y_voltage);
+				
+				//printf("%s, %f\n\r", x_voltage, y_voltage);
+				//printf("%f, %f\n\r", x_volts, y_volts);
+					
+			//	printf("Y%s\n\rX%s\n\r",y_voltage,x_voltage);
+			//	printf("\n%f, %f, %f, %f\r", y_volts, y_check, x_volts, x_check);
+				//printf("X: %i, Y: %i\r", x_volts,y_volts);
+				
+				//x_volts = 1.65;
+				//y_volts = 3.3;
+				
+				y_power = (y_volts-1.65)*154.54;
+				x_power = (x_volts-1.65)*154.54;
+				
+				if(abs(x_power) <= abs(y_power))
+				{
+					power = abs(y_power);
+				}
+				else
+				{
+					power = abs(x_power);
+				}
+				
+				if (x_volts < 1.62) 
+				{
+			    	//Thumbstick is moved to the left
+			    	if(y_power >= -0.02)
+			    	{
+			    		leftb_pwm = 0;
+			        	rightb_pwm = 0;
+				    	rightf_pwm = power;
+					    leftf_pwm = (x_volts * (power) / 1.65); 
+					}
+					else
+					{
+						leftf_pwm = 0;
+			        	rightf_pwm = 0;
+						rightb_pwm = power;
+					    leftb_pwm = (x_volts * (power) / 1.65); 
+					}
+			    }
+			    else if ( x_volts > 1.67 ) 
+			    {
+			        // Thumbstick is moved to the right
+			        if(y_power >= -0.02)
+			        {
+			        	leftb_pwm = 0;
+			        	rightb_pwm = 0;
+				        leftf_pwm = power;
+					    rightf_pwm = power - ((x_volts-1.65) * (power) / 1.65);
+					}
+					else
+					{
+						leftf_pwm = 0;
+			        	rightf_pwm = 0;
+						leftb_pwm = power;
+					    rightb_pwm = power - ((x_volts-1.65) * (power) / 1.65); 
+					}
+			    }
+			    else if ((1.62 <= x_volts && x_volts <= 1.67) && (1.62 <= y_volts && y_volts <= 1.67))
+			    {
+			        // Thumbstick is in the middle, stop both motors
+			        leftf_pwm = 0;
+			        rightf_pwm = 0;
+			        leftb_pwm = 0;
+			        rightb_pwm = 0;
+			    }
+			    else 
+			    {
+			    	if(y_power >= -0.02)
+			        {
+			        	leftb_pwm = 0;
+			        	rightb_pwm = 0;
+				    	leftf_pwm = power;
+				    	rightf_pwm = power;
+				    }
+				    else
+				    {
+				    	leftf_pwm = 0;
+			        	rightf_pwm = 0;
+				    	leftb_pwm = power;
+				    	rightb_pwm = power;
+				    }
+			    }
+			
+		}
+			   //printf("\rHIGH, %i, %i, \n", leftf_pwm, rightf_pwm);
+			   //printf("LOW, %i, %i, \n", leftb_pwm, rightb_pwm);
+		
+		
+	}
+
+}
