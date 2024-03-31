f06a7186254c0029ebd6f51b4c58a6ef931e058f
Taranehhhhhhhh <115417532+Taranehhhhhhhh@users.noreply.github.com>
2024-03-30 19:14:43 -0700

    Add files via upload

---
+++ TimerIRQ.c
@@ -0,0 +1,113 @@
+#include <XC.h>
+#include <sys/attribs.h>
+#include <stdio.h>
+
+#pragma config FNOSC = FRCPLL       // Internal Fast RC oscillator (8 MHz) w/ PLL
+#pragma config FPLLIDIV = DIV_2     // Divide FRC before PLL (now 4 MHz)
+#pragma config FPLLMUL = MUL_20     // PLL Multiply (now 80 MHz)
+#pragma config FPLLODIV = DIV_2     // Divide After PLL (now 40 MHz) see figure 8.1 in datasheet for more info
+#pragma config FWDTEN = OFF         // Watchdog Timer Disabled
+#pragma config FSOSCEN = OFF        // Secondary Oscillator Enable (Disabled).  A4 and B4 can used as GPIO.
+#pragma config FPBDIV = DIV_1       // PBCLK = SYCLK
+#define SYSCLK 40000000L
+#define Baud2BRG(desired_baud)( (SYSCLK / (16*desired_baud))-1)
+
+
+/* Pinout for DIP28 PIC32MX130:
+                                          --------
+                                   MCLR -|1     28|- AVDD 
+  VREF+/CVREF+/AN0/C3INC/RPA0/CTED1/RA0 -|2     27|- AVSS 
+        VREF-/CVREF-/AN1/RPA1/CTED2/RA1 -|3     26|- AN9/C3INA/RPB15/SCK2/CTED6/PMCS1/RB15
+   PGED1/AN2/C1IND/C2INB/C3IND/RPB0/RB0 -|4     25|- CVREFOUT/AN10/C3INB/RPB14/SCK1/CTED5/PMWR/RB14
+  PGEC1/AN3/C1INC/C2INA/RPB1/CTED12/RB1 -|5     24|- AN11/RPB13/CTPLS/PMRD/RB13
+   AN4/C1INB/C2IND/RPB2/SDA2/CTED13/RB2 -|6     23|- AN12/PMD0/RB12
+     AN5/C1INA/C2INC/RTCC/RPB3/SCL2/RB3 -|7     22|- PGEC2/TMS/RPB11/PMD1/RB11
+                                    VSS -|8     21|- PGED2/RPB10/CTED11/PMD2/RB10
+                     OSC1/CLKI/RPA2/RA2 -|9     20|- VCAP
+                OSC2/CLKO/RPA3/PMA0/RA3 -|10    19|- VSS
+                         SOSCI/RPB4/RB4 -|11    18|- TDO/RPB9/SDA1/CTED4/PMD3/RB9
+         SOSCO/RPA4/T1CK/CTED9/PMA1/RA4 -|12    17|- TCK/RPB8/SCL1/CTED10/PMD4/RB8
+                                    VDD -|13    16|- TDI/RPB7/CTED3/PMD5/INT0/RB7
+                    PGED3/RPB5/PMD7/RB5 -|14    15|- PGEC3/RPB6/PMD6/RB6
+                                          --------
+*/
+
+void UART2Configure(int baud_rate)
+{
+    // Peripheral Pin Select
+    U2RXRbits.U2RXR = 4;    //SET RX to RB8
+    RPB9Rbits.RPB9R = 2;    //SET RB9 to TX
+
+    U2MODE = 0;         // disable autobaud, TX and RX enabled only, 8N1, idle=HIGH
+    U2STA = 0x1400;     // enable TX and RX
+    U2BRG = Baud2BRG(baud_rate); // U2BRG = (FPb / (16*baud)) - 1
+    
+    U2MODESET = 0x8000;     // enable UART2
+}
+
+void wait_1ms(void)
+{
+    unsigned int ui;
+    _CP0_SET_COUNT(0); // resets the core timer count
+
+    // get the core timer count
+    while ( _CP0_GET_COUNT() < (SYSCLK/(2*1000)) );
+}
+
+void delayms(int len)
+{
+	while(len--) wait_1ms();
+}
+
+void __ISR(_TIMER_1_VECTOR, IPL5SOFT) Timer1_Handler(void)
+{
+	LATAbits.LATA1 = !LATAbits.LATA1; // Blink led on RB6
+	IFS0CLR = _IFS0_T1IF_MASK; // Clear timer 1 interrupt flag, bit 4 of IFS0
+}
+
+void SetupTimer1 (long FREQ)
+{
+	// Explanation here:
+	// https://www.youtube.com/watch?v=bu6TTZHnMPY
+	__builtin_disable_interrupts();
+	PR1 =(SYSCLK/(FREQ*256))-1; // since SYSCLK/FREQ = PS*(PR1+1)
+	TMR1 = 0;
+	T1CONbits.TCKPS = 3; // Pre-scaler: 256
+	T1CONbits.TCS = 0; // Clock source
+	T1CONbits.ON = 1;
+	IPC1bits.T1IP = 5;
+	IPC1bits.T1IS = 0;
+	IFS0bits.T1IF = 0;
+	IEC0bits.T1IE = 1;
+	
+	INTCONbits.MVEC = 1; //Int multi-vector
+	__builtin_enable_interrupts();
+}
+
+void main (void)
+{
+	long FREQ = 100L; 
+	DDPCON = 0;
+	
+    ANSELAbits.ANSA1 = 0; // Disable analog function on RA1
+    TRISAbits.TRISA1 = 0; // Set RA1 as output
+    LATAbits.LATA1 = 0;   // Initialize RA1 to low
+    
+    INTCONbits.MVEC = 1; // Enable multi-vector interrupts
+    while(1){
+    	while(FREQ <= 8000L){
+		    FREQ = FREQ + 1; 
+		 	wait_1ms(); 
+			SetupTimer1(FREQ);
+			UART2Configure(115200);  // Configure UART2 for a baud rate of 115200
+	
+ 			printf("%ld\r", FREQ);
+		} 
+		FREQ =4000L;
+	}
+	
+	while (1)
+	{
+	}
+}
+
