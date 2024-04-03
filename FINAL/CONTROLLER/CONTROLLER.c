a01d94194c87f8892bf90c20b21aa626eff1197e
DreSaid <145886577+DreSaid@users.noreply.github.com>
2024-04-02 22:02:55 -0700

    Update CONTROLLER.c
    speak

--- FINAL/CONTROLLER/CONTROLLER.c
+++ FINAL/CONTROLLER/CONTROLLER.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
 /* Pinout for DIP28 PIC32MX130:
                                           --------
                                    MCLR -|1     28|- AVDD 
@@ -39,17 +37,14 @@
 #define Baud2BRG(desired_baud)( (SYSCLK / (16*desired_baud))-1)
 #define Baud1BRG(desired_baud)( (SYSCLK / (16*desired_baud))-1)
 #define BASE_FREQ 2048L // 2Hz or 0.5 seconds interrupt rate
-
 //LCD defines
 #define CHARS_PER_LINE 16
-
 #define LCD_D7_ENABLE TRISAbits.TRISA4
 #define LCD_D6_ENABLE TRISBbits.TRISB4   
 #define LCD_D5_ENABLE TRISAbits.TRISA3   
 #define LCD_D4_ENABLE TRISAbits.TRISA2  
 #define LCD_E_ENABLE  TRISBbits.TRISB3   
 #define LCD_RS_ENABLE TRISAbits.TRISA0
-
 #define LCD_RS LATAbits.LATA0
 #define LCD_E LATBbits.LATB3
 #define LCD_D4 LATAbits.LATA2
@@ -57,16 +52,17 @@
 #define LCD_D6 LATBbits.LATB4
 #define LCD_D7 LATAbits.LATA4
 
+//FREQUENCY CHANGE REF SO WE CAN FIND IT
+#define FCHANGE_REF 600
+
 
 void wait_1us(void);
 void delayus(int);
-
 void UART2Configure(int baud_rate)
 {
     // Peripheral Pin Select
     U2RXRbits.U2RXR = 4;    //SET RX to RB8
     RPB9Rbits.RPB9R = 2;    //SET RB9 to TX
-
     U2MODE = 0;         // disable autobaud, TX and RX enabled only, 8N1, idle=HIGH
     U2STA = 0x1400;     // enable TX and RX
     U2BRG = Baud2BRG(baud_rate); // U2BRG = (FPb / (16*baud)) - 1
@@ -94,12 +90,9 @@ void SetupTimer1 (int FREQ)
 	IPC1bits.T1IS = 0;
 	IFS0bits.T1IF = 0;
 	IEC0bits.T1IE = 1;
-
 	INTCONbits.MVEC = 1; //Int multi-vector
 	__builtin_enable_interrupts();
 }
-
-
 // Needed to by scanf() and gets()
 int _mon_getc(int canblock)
 {
@@ -128,8 +121,6 @@ int _mon_getc(int canblock)
         }
     }
 }
-
-
 void ADCConf(void)
 {
     AD1CON1CLR = 0x8000;    // disable ADC before configuration
@@ -138,7 +129,6 @@ void ADCConf(void)
     AD1CON3 = 0x0f01;       // TAD = 4*TPB, acquisition time = 15*TAD 
     AD1CON1SET=0x8000;      // Enable ADC
 }
-
 int ADCRead(char analogPIN)
 {
     AD1CHS = analogPIN << 16;    // AD1CHS<16:19> controls which analog pin goes to the ADC
@@ -149,27 +139,20 @@ int ADCRead(char analogPIN)
  
     return ADC1BUF0;             // result stored in ADC1BUF0
 }
-
-
-
 /////////////////////////////////////////////////////////
 // UART1 functions used to communicate with the JDY40  //
 /////////////////////////////////////////////////////////
-
 // TXD1 is in pin 26  aka RXD aka RB15
 // RXD1 is in pin 24  aka TXD aka RB13
-
 int UART1Configure(int desired_baud)
 {
 	int actual_baud;
-
     // Peripheral Pin Select for UART1.  These are the pins that can be used for U1RX from TABLE 11-1 of '60001168J.pdf':
     // 0000 = RPA2
 	// 0001 = RPB6
 	// 0010 = RPA4
 	// 0011 = RPB13
 	// 0100 = RPB2
-
 	// Do what the caption of FIGURE 11-2 in '60001168J.pdf' says: "For input only, PPS functionality does not have
     // priority over TRISx settings. Therefore, when configuring RPn pin for input, the corresponding bit in the
     // TRISx register must also be configured for input (set to ‘1’)."
@@ -178,14 +161,12 @@ int UART1Configure(int desired_baud)
     TRISB |= (1<<13);   // configure pin RB13 as input
     CNPUB |= (1<<13);   // Enable pull-up resistor for RB13
     U1RXRbits.U1RXR = 3; // SET U1RX to RB13
-
     // These are the pins that can be used for U1TX. Check table TABLE 11-2 of '60001168J.pdf':
     // RPA0
 	// RPB3
 	// RPB4
 	// RPB15
 	// RPB7
-
     ANSELB &= ~(1<<15); // Set RB15 as a digital I/O
     RPB15Rbits.RPB15R = 1; // SET RB15 to U1TX
 	
@@ -194,9 +175,7 @@ int UART1Configure(int desired_baud)
     U1BRG = Baud1BRG(desired_baud); // U1BRG = (FPb / (16*baud)) - 1
     // Calculate actual baud rate
     actual_baud = SYSCLK / (16 * (U1BRG+1));
-
     U1MODESET = 0x8000;     // enable UART1
-
     return actual_baud;
 }
  
@@ -228,7 +207,7 @@ unsigned int SerialReceive1(char *buffer, unsigned int max_size)
         {
         	delayus(100);
         	timeout_counter++;
-        	if(timeout_counter==100)
+        	if(timeout_counter==1000)
         	{
         		printf("TIMEOUT");
         		*buffer = '\0';
@@ -250,36 +229,29 @@ unsigned int SerialReceive1(char *buffer, unsigned int max_size)
  
     return num_char;
 }
-
 // Use the core timer to wait for 1 ms.
 void wait_1ms(void)
 {
     unsigned int ui;
     _CP0_SET_COUNT(0); // resets the core timer count
-
     // get the core timer count
     while ( _CP0_GET_COUNT() < (SYSCLK/(2*1000)) );
 }
-
 void delayms(int len)
 {
 	while(len--) wait_1ms();
 }
-
 void wait_1us(void)
 {
     unsigned int ui;
     _CP0_SET_COUNT(0); // resets the core timer count
-
     // get the core timer count
     while ( _CP0_GET_COUNT() < (SYSCLK/(2*1000000)) );
 }
-
 void delayus(int len)
 {
 	while(len--) wait_1us();
 }
-
 //LCD CODE//
 void LCD_pulse(void)
 {
@@ -287,7 +259,6 @@ void LCD_pulse(void)
 	delayus(40);
 	LCD_E = 0;
 }
-
 void LCD_byte(unsigned char x)
 {
 	LCD_D7=(x&0x80)?1:0;
@@ -302,21 +273,18 @@ void LCD_byte(unsigned char x)
 	LCD_D4=(x&0x01)?1:0;
 	LCD_pulse();
 }
-
 void WriteData(unsigned char x)
 {
 	LCD_RS = 1;
 	LCD_byte(x);
 	delayms(2);
 }
-
 void WriteCommand(unsigned char x)
 {
 	LCD_RS = 0;
 	LCD_byte(x);
 	delayms(5);
 }
-
 void LCD_4BIT(void)
 {
 	// Configure the pins used to communicate with the LCD as outputs
@@ -342,7 +310,6 @@ void LCD_4BIT(void)
 	delayms(20); // Wait for clear screen command to finish
 	LATBbits.LATB0 = 	!LATBbits.LATB0;
 }
-
 void LCDprint(char * string, unsigned char line, unsigned char clear)
 {
 	int j;
@@ -355,7 +322,6 @@ void LCDprint(char * string, unsigned char line, unsigned char clear)
 		for(;j<CHARS_PER_LINE;j++)
 			WriteData(' '); //Clear the rest of the line if clear is 1
 }
-
 void SendATCommand (char * s)
 {
 	char buff[40];
@@ -368,7 +334,6 @@ void SendATCommand (char * s)
 	delayms(10);
 	printf("Response: %s\n", buff);
 }
-
 void main(void)
 {
 	char buff[80];
@@ -385,52 +350,54 @@ void main(void)
     int timeout;
     int timeout_cnt = 0;
     int received_value; //this is a TESTING variable
+    int count =0;
     int freq_change;
-    
     
  	
     
 	DDPCON = 0;
 	CFGCON = 0;
 	
+	//SPEAKER STUFF
 	ANSELAbits.ANSA1 = 0; // Disable analog function on RA1
     TRISAbits.TRISA1 = 0; // Set RA1 as output
     LATAbits.LATA1 = 0;   // Initialize RA1 to low
+    
+    ANSELA &= ~(1<<1);	//Set RA1 as a digital I/O
+    TRISA &= ~(1<<1); 	//configure pin RA1 as output (speaker)
+
+    //TRISAbits.TRISA1 = 0; // Set RA1 as output
+    //LATAbits.LATA1 = 0;   // Initialize RA1 to low
 
     INTCONbits.MVEC = 1; // Enable multi-vector interrupts
 	SetupTimer1(BASE_FREQ);
 	LCD_4BIT();
-	
+
 	// Configure pins as analog inputs
-    ANSELBbits.ANSB0 = 1;   // set RB1 (AN4, pin 5 of DIP28) as analog pin	
-    TRISBbits.TRISB0 = 1;   // set RB1 as an input
-    
+    //ANSELBbits.ANSB1 = 1;   // set RB1 (AN4, pin 5 of DIP28) as analog pin	
+    //TRISBbits.TRISB1 = 1;   // set RB1 as an input
+    ANSELBbits.ANSB0 = 1;   // set RB0 (AN2, pin 3 of DIP28) as analog pin	
+    TRISBbits.TRISB0 = 1;   // set RB0 as an input
+
     ANSELBbits.ANSB2 = 1;
     TRISBbits.TRISB2 = 1;	//set RB2 as input (x direction)
 	
 	ADCConf(); // Configure ADC
-
   
     UART2Configure(115200);  // Configure UART2 for a baud rate of 115200
     UART1Configure(9600);  // Configure UART1 to communicate with JDY40 with a baud rate of 9600
  
 	delayms(500); // Give putty time to start before we send stuff.
     printf("JDY40 test program.\r\n");
-
 	// RB14 is connected to the 'SET' pin of the JDY40.  Configure as output:
     ANSELB &= ~(1<<14); // Set RB14 as a digital I/O
     TRISB &= ~(1<<14);  // configure pin RB14 as output
 	LATB |= (1<<14);    // 'SET' pin of JDY40 to 1 is normal operation mode
-	
-	
-	//SPEAKER STUFF
-	//TRISAbits.TRISA1 = 1;   // Set RA1 as output
-    //LATAbits.LATA1 = 0;     // Ensure RA1 is initially low
+
 
 	// We should select an unique device ID.  The device ID can be a hex
 	// number from 0x0000 to 0xFFFF.  In this case is set to 0xABBA
-	SendATCommand("AT+DVID6969\r\n");  
-
+	SendATCommand("AT+DVID6058\r\n");  
 	// To check configuration
 	SendATCommand("AT+VER\r\n");
 	SendATCommand("AT+BAUD\r\n");
@@ -439,88 +406,93 @@ void main(void)
 	SendATCommand("AT+RFC\r\n");
 	SendATCommand("AT+POWE\r\n");
 	SendATCommand("AT+CLSS\r\n");           
-
     ANSELB &= ~(1<<6); // Set RB6 as a digital I/O
     TRISB |= (1<<6);   // configure pin RB6 as input
     CNPUB |= (1<<6);   // Enable pull-up resistor for RB6
-    
-    //ANSELA &= ~(1<<1);	//Set RA1 as a digital I/O
-    ///TRISA &= ~(1<<1); 	//configure pin RA1 as output (speaker)
- 	
+
+
+
 	printf("\r\nPress and hold a push-button attached to RB6 (pin 15) to transmit.\r\n");
-	
+
 	cnt=0;
 	LCDprint("big n greedy", 1,1);
 	while(1)
 	{
-		timeout=0;
-		//if((PORTB&(1<<6))==0)
-		//{
+		//MEASURE PINS FROM JOYSTICK
 		adcval_y = ADCRead(4); // note that we call pin AN4 (RB2) by it's analog number
     	voltage_y=adcval_y*3.3/1023.0;
     	adcval_x = ADCRead(2); 	//reading from AN3 (RB1)
     	voltage_x = adcval_x*3.3/1023.0;
+    	
 		
+		//FIRST SECTION SENDS AN M when we want a metal reading--how often depends on count variable and delays
+		
+		//if((PORTB&(1<<6))==0)
+		if(count>=5)
+		{
+		count=0; //reset count every time M is sent
 		sprintf(buff, "YM%.3f\nX%.3f\r\n", voltage_y, voltage_x);
 		//printf("%d", strlen(buff));
-		printf("%s\n", buff);
+		//printf("%s\n", buff);
 		SerialTransmit1(buff);
 		//printf("sent");
-		delayms(15);
-	
+
 		timeout_cnt=0;
 		while(1) {
 			if(U1STAbits.URXDA) break; //Got something! get out of loop
 			delayus(100); //check if smth has arrived
 			timeout_cnt++;
-			if(timeout_cnt>=100) break; //timeout after 10 ms
-			//printf("stuck1");
+			if(timeout_cnt>=1000) break; //timeout after x ms
+			//printf("waiting");
+
 		}
-		if(U1STAbits.URXDA) {// Something has arrived
-			printf("arrived");
+		
+		if(U1STAbits.URXDA) 	// Something has arrived
+		{		
+			//printf("arrived");
 			//delayms(100);
 			SerialReceive1(buff, sizeof(buff)-1);
-			printf("Received_val: %s\r\n", buff);
-			delayms(10);
+			//printf("\nReceived_val: %s\r\n", buff);
+			//printf("%s\r\n", buff);
+			delayms(10); //Important(?)
 			//printf("received\r\n");
-			//if(strlen(buff)==9) //assuming a message from robot is 5 bytes
-			//{
+			if(strlen(buff)==4) //assuming a message from robot is 5 bytes
+			{
 				//printf("in\n");
 				freq_change = atoi(buff);
-				//printf("%d\r\n",freq_change);
-
-			//}
+				printf("%d\r\n",freq_change);
+			}
 		}	
-			
-		/*} else {
-		adcval_y = ADCRead(4); // note that we call pin AN4 (RB2) by it's analog number
-    	voltage_y=adcval_y*3.3/1023.0;
-    	adcval_x = ADCRead(3); 	//reading from AN3 (RB1)
-    	voltage_x = adcval_x*3.3/1023.0;
-    	
-    	sprintf(buff, "Y%.3f\nX%.3f\r\n", voltage_y, voltage_x);
+
+		} else {
+
+		sprintf(buff, "Y%.3f\nX%.3f\r\n", voltage_y, voltage_x);
 		//printf("%d", strlen(buff));
-		printf("line %s\n", buff);
+		//printf("%s\n", buff);
 		SerialTransmit1(buff);
 		
-		delayms(15);            
-    	}*/
+		
+		//delayms(10);            
+    	}
+    //	printf("%.3f %.3f\r", voltage_y, voltage_x);
+    //	}
     	//printf("%.3f %.3f\r", voltage_y, voltage_x);
- 	  	 // Makes the printf() above to send without a '\n' at the end
-
-
-		//speaker code
-		freq_change=100; //MANUALLY CHANGING FREQ TMRW
-		if (freq_change > 160) 
+ 	  	 // Makes the printf() above to send without a '\n' at the end	
+	//printf("loop");
+		//SPEAKER CODE
+		//freq_change=100;
+		if (freq_change > FCHANGE_REF) 
 		{
-			SetupTimer1(freq_change); //(this function also turns speaker on)
+			SetupTimer1(freq_change*4); //(this function also turns speaker on)
 			//WE NEED TO WRITE AN EQUATION FOR THIS PARAMETER, maybe use BASE_FREQ
 		}
 		else
 		{
 			T1CONbits.ON = 0; //speaker off otherwise
 		}
-			
-	//printf("loop");   //debugger
+
+		
+	
+		count++;
 	}
 }
